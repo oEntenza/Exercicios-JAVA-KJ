b72677e4441f85e42fccb7e54b00b934d6571ece
oEntenza <111072453+oEntenza@users.noreply.github.com>
2024-03-24 12:02:57 -0300

    Adicionando 3 exercicios ao repositório

---
+++ exercicioskj/src/exercicioskj/JogoAdivinhacao.java
@@ -0,0 +1,40 @@
+package exercicioskj;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class JogoAdivinhacao {
+
+	private int numeroSorteado;
+    private Random random;
+    
+    public JogoAdivinhacao() {
+        random = new Random();
+    }
+    
+    public void sortear() {
+        numeroSorteado = random.nextInt(100);
+    }
+    
+    public void adivinhar() {
+        Scanner scanner = new Scanner(System.in);
+        int tentativas = 0;
+        int palpite;
+        
+        do {
+            System.out.print("Digite seu palpite (entre 0 e 99): ");
+            palpite = scanner.nextInt();
+            tentativas++;
+            
+            if (palpite < numeroSorteado) {
+                System.out.println("Palpite muito baixo! Tente novamente.");
+            } else if (palpite > numeroSorteado) {
+                System.out.println("Palpite muito alto! Tente novamente.");
+            } else {
+                System.out.println("Parabéns! Você acertou o número em " + tentativas + " tentativas.");
+            }
+        } while (palpite != numeroSorteado);
+       
+        scanner.close();
+    }	
+}
---
+++ exercicioskj/src/exercicioskj/Main.java
@@ -0,0 +1,45 @@
+package exercicioskj;
+
+public class Main {
+
+    /*
+     * EXERCICIO 1
+     * 
+     * public static void main(String[] args) {
+        Pessoa pessoa1 = new Pessoa("Joao","Masculino",21,false);
+        Pessoa pessoa2 = new Pessoa("Kaily","Feminina",21,false);
+        Pessoa pessoa3 = new Pessoa("Renato","Masculino",10,true);
+        Pessoa pessoa4 = new Pessoa("Juliana","Feminino",3,false);
+        
+        Churrasco churrasco = new Churrasco(0);
+        
+        churrasco.verificarConsumo(pessoa1);
+        churrasco.verificarConsumo(pessoa2);
+        churrasco.verificarConsumo(pessoa3);
+        churrasco.verificarConsumo(pessoa4);
+        
+        double quantidadeTotalCarne = churrasco.getQtCarne();
+
+        System.out.println("Quantidade total de carne consumida: " + String.format("%.0f", quantidadeTotalCarne) + " kg");
+    }*/
+	
+    /*
+     * EXERCICIO 2
+     * public static void main(String[] args) {
+        JogoAdivinhacao jogo = new JogoAdivinhacao();
+        jogo.sortear();
+        jogo.adivinhar();
+    }*/
+    
+	/*
+	 * EXERCICIO 3
+	 * public static void main(String[] args) {
+   	 Cliente cliente1 = new Cliente("Joaozin gameplays", 1000, 500);
+        System.out.println("Saldo + Limite: " + cliente1.checarSaldo());
+        cliente1.sacar(1500);
+        System.out.println("Saldo + Limite: " + cliente1.checarSaldo());
+        cliente1.depositar(200);
+        System.out.println("Saldo + Limite: " + cliente1.checarSaldo());
+   }*/
+
+}

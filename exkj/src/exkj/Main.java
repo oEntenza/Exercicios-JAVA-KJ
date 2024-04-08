4c0f99d93a79409bf1914e02f3b409da95361f08
Kailane196 <kailanerodrigues196@gmail.com>
2024-04-08 11:01:11 -0300

    corrigindo erros

---
+++ exkj/src/exkj/Main.java
@@ -0,0 +1,71 @@
+package exkj;
+
+public class Main {
+
+    /*
+     * EXERCICIO 1
+     * 
+     public static void main(String[] args) {
+        Pessoa pessoa1 = new Pessoa("Joao","Masculino",21,false);
+        Pessoa pessoa2 = new Pessoa("Kaily","Feminina",21,false);
+        Pessoa pessoa3 = new Pessoa("Renato","Masculino",10,true);
+        Pessoa pessoa4 = new Pessoa("Juliana","Feminino",3,false);
+        
+        Churrasco churrasco = new Churrasco(0);
+        
+        churrasco.verificarConsumo(pessoa1);
+        churrasco.verificarConsumo(pessoa2);
+        churrasco.verificarConsumo(pessoa3);
+        churrasco.verificarConsumo(pessoa4);
+        
+        double quantidadeTotalCarne = churrasco.getQtCarne();
+
+        System.out.println("Quantidade total de carne consumida: " + String.format("%.0f", quantidadeTotalCarne) + " kg");
+    }*/
+	
+    /*
+     * EXERCICIO 6
+     *
+     public static void main(String[] args) {
+        JogoAdivinhacao jogo = new JogoAdivinhacao();
+        jogo.sortear();
+        jogo.adivinhar();
+    }*/
+    
+	/*
+	 * EXERCICIO 4
+	 *
+	 public static void main(String[] args) {
+   	 	Cliente cliente1 = new Cliente("Joaozin gameplays", 1000, 500);
+        System.out.println("Saldo + Limite: " + cliente1.checarSaldo());
+        cliente1.sacar(1500);
+        System.out.println("Saldo + Limite: " + cliente1.checarSaldo());
+        cliente1.depositar(200);
+        System.out.println("Saldo + Limite: " + cliente1.checarSaldo());
+   }*/
+
+	/*
+ 	 * EXERCICIO 5
+   	 * 
+     public static void main(String[] args) {
+        double lado = 5.0;
+        TrianguloEquilatero triangulo = new TrianguloEquilatero(lado);
+        
+        System.out.println("Lado do triângulo: " + triangulo.getLado());
+        System.out.println("Área do triângulo: " + String.format("%.2f", triangulo.getArea()));
+        System.out.println("Perímetro do triângulo: " + triangulo.getPerimetro());
+    }*/
+
+    /*
+     * EXERCICIO 7
+     *
+    public static void main(String[] args) { // Método principal
+        // Exemplo de uso da classe Eq2Grau
+        Eq2Grau equacao = new Eq2Grau(1, -3, 2); // Cria um objeto Eq2Grau com os coeficientes especificados
+        System.out.println("Delta: " + equacao.delta()); // Imprime o valor do delta da equação
+        System.out.println("Raiz 1: " + equacao.raiz1()); // Imprime o valor da primeira raiz da equação
+        System.out.println("Raiz 2: " + equacao.raiz2()); // Imprime o valor da segunda raiz da equação
+    }*
+     * 
+     */
+}
